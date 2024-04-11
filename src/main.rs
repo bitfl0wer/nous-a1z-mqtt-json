7f374cb2eb1d96f509caa7ff3d936032fac5325d
bitfl0wer <florian@pro-weber.com>
2024-04-11 11:20:45 +0200

    Use lazy_static for global access to Args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
 
+lazy_static::lazy_static! {
+    static ref CLI_ARGS: Args = Args::parse();
+}
+
 /// Query "nous A1Z" smart plugs exposed via Zigbee2MQTT, accumulate the data
 /// and host it as JSON data using a simple web server so that it can be
 /// imported into Grafana.
@@ -7,7 +11,7 @@ use clap::Parser;
 #[command(version, about, long_about)]
 struct Args {
     /// The MQTT server URL. Example: mqtt://localhost:1833
-    server: String,
+    pub server: String,
     /// Topic where the smart plugs are exposed under
     topic: String,
     /// Username for authorization, if applicable
@@ -20,8 +24,8 @@ struct Args {
     friendly_names: Vec<String>,
 }
 
-fn main() {
-    let _args = Args::parse();
-
-    println!("Hello, world!");
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let _ = Args::parse(); // Ensure Args have been given correctly, lazy_static does not seem to invoke Args::parse() in a way which would halt execution if an argument is missing/incorrect
+                           // We can use `CLI_ARGS` after this.
 }
